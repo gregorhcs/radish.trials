daa13fd7d31473095bdfd74151f8455649a3f505
culorin <culorin@users.noreply.github.com>
2022-07-18 00:36:00 +0200

    T8: ~ fix! scene 1 wrong positioning

--- modTrial8/full.rebuild.bat
+++ modTrial8/full.rebuild.bat
@@ -9,6 +9,6 @@ rem ---------------------------------------------------
 SET INTERACTIVE_BUILD=0
 SET PATCH_MODE=0
 SET FULL_REBUILD=1
-SET START_GAME=0
+SET START_GAME=1
 
 call "%DIR_PROJECT_BIN%\build.bat"
